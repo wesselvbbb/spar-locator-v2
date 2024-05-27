8a725b9f32c8ffd4a72a252741c0ffb5714f097d
wesselvanbeek <wessel.van.beek@dpdk.com>
2024-05-27 14:13:44 +0200

    Base + NativeWind

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://stud.hosted.hr.nl/1027694/Programmeren/PRG07/Spar-Locator/webservice.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data.items);
+      })
+      .catch((error) => {
+        console.error(error);
+        setItems([]);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {items.map((item, index) => (
+        <View key={index}>
+          <Text className="text-red-500">{item.title}</Text>
+          <Text className="text-red-500">{item.description}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
